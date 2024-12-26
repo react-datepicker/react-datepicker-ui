76f61ba87e828e06121532b2c3f9ff3449b72294
Ariel Mints <arikm9@gmail.com>
2024-12-26 12:28:21 +0200

    Vite lib mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
-import path from "path";
+import path, { resolve } from "path";
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "@react-datepicker/ui",
+      // the proper extensions will be added
+      fileName: "react-datepicker-ui",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
